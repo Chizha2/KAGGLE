449603d195ca72ce08386e1cbfebbe4044cb97f4
dgok50 <dgok50@gmail.com>
2018-03-30 01:58:41 +0300

    remove non valed data

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -5,5 +5,12 @@ le = LabelEncoder()
 file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
 file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
 
+
+for i in file.head():
+   if(int(file[i].notnull().sum() / len(file) * 100) < 80):
+       del file[i]
+
+
 for i in file.head():
     print(str(i) + ", " + str(int(file[i].notnull().sum() / len(file) * 100)) + "%") # рассчитываем коэффициент валидных данных
+
