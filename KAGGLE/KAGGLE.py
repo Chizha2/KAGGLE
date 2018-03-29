70dfcd15511eea93584167b72e6bef31a526e06d
dgok50 <dgok50@gmail.com>
2018-03-30 02:57:31 +0300

    fix cosak

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -4,12 +4,14 @@ le = LabelEncoder()
 
 file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
 
+
 for i in file.head():
     if int(file[i].notnull().sum() / len(file) * 100) < 80: # рассчитываем коэффициент валидных данных
         del file[i]
     else:
         file[i] = file[i].fillna(file[i].value_counts().idxmax())
 
+#file = file.fillna(method='pad')
 
 # file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
 
