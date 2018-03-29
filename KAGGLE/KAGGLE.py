d96385f1144b2b9adcfeb800b4e574365809e132
Chizh <Chizha2@gmail.com>
2018-03-29 20:28:02 +0300

    велик

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -1,5 +1,11 @@
-import pandas as pd # для обработки даных
+import pandas as pd # для обработки данных
 file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
 file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
-print(file_obj["Alley"].value_counts())
-print("Vsego: " + str(file["Alley"].sum()) + ", nan: " + str(file["Alley"].isnull().sum()))
+
+for h in file.head():
+    k = 0
+    print(h)
+    for i in file[h]:
+        if pd.notnull(i):
+           k += 1
+    print(k / len(file))
