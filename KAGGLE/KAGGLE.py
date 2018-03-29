19140bbe19df9abe972d6c4b31f679cbc771b80c
Chizh <Chizha2@gmail.com>
2018-03-29 19:03:43 +0300

    nan

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -1,3 +1,5 @@
-import pandas as pd
-file = pd.read_csv('../zadanie/train.csv')
-print(file.head())
+import pandas as pd # для обработки даных
+file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
+file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
+print(file_obj["Alley"].value_counts())
+print("Vsego: " + str(file["Alley"].sum()) + ", nan: " + str(file["Alley"].isnull().sum()))
