119cd27fe9f93fb39d3682d46c6994cf483f8777
dgok50 <dgok50@gmail.com>
2018-03-29 20:26:59 +0300

    valid cof add

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -1,5 +1,9 @@
 import pandas as pd # для обработки даных
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+
 file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
 file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
-print(file_obj["Alley"].value_counts())
-print("Vsego: " + str(pd.to_numeric(file["Alley"], errors='coerce').sum()) + ", nan: " + str(pd.to_numeric(file["Alley"].isnull(), errors='coerce').sum()))
+
+for i in file.head():
+    print("Head: "+ str(i) + ", valid: " + str( file[i].notnull().sum()/len(file) )) # рассчитываем коэффициент валидных данных
