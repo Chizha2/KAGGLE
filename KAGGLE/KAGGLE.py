8e9bb4255ad436368302b07a1bf8b91935a9aff9
dgok50 <dgok50@gmail.com>
2018-03-30 03:17:53 +0300

    sklearn encode lable add

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -1,9 +1,11 @@
 import pandas as pd # для обработки данных
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
+from collections import defaultdict
+d = defaultdict(LabelEncoder)
 
 file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
-
 
 for i in file.head():
     if int(file[i].notnull().sum() / len(file) * 100) < 80: # рассчитываем коэффициент валидных данных
@@ -15,5 +17,16 @@ for i in file.head():
 
 # file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
 
-for i in file.select_dtypes(include=['object']):
-    if
+#for i in file.select_dtypes(include=['object']):
+file_obj = file.select_dtypes(include=['object'])
+#file_obj = file
+
+print(file_obj["LotShape"])
+
+file_obj = file_obj.apply(lambda x: d[x.name].fit_transform(x))
+
+print(file_obj["LotShape"])
+
+file_obj = file_obj.apply(lambda x: d[x.name].inverse_transform(x))
+
+print(file_obj["LotShape"])
