95ca21cdb132d1e7ac404369773e1463ef9015ac
Chizh <Chizha2@gmail.com>
2018-03-31 16:49:21 +0300

    1. Рефакторинг. 2. Очистка предупреждений.

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -1,32 +1,25 @@
-import pandas as pd # для обработки данных
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-from collections import defaultdict
-d = defaultdict(LabelEncoder)
+import pandas as pd # для обработки и анализа данных
+import numpy as np # для вычислительных алгоритмов
+from sklearn.preprocessing import LabelEncoder # для трансформаций
+# le = LabelEncoder()                                                                                                                    ЗАЧЕМ?
+from collections import defaultdict # для метода словаря
+import warnings # для настройки предупреждений
+warnings.filterwarnings("ignore") # отключение предупреждений
+d = defaultdict(LabelEncoder) # создание словаря для трансформаций
 
-file = pd.read_csv('../zadanie/train.csv', na_values="NA") # чтение файла
+file = pd.read_csv("../zadanie/train.csv", na_values = "NA") # чтение файла, пустые значения = "NA"
 
-for i in file.head():
-    if int(file[i].notnull().sum() / len(file) * 100) < 80: # рассчитываем коэффициент валидных данных
-        del file[i]
+for i in file.head(): # по столбцам
+    if int(file[i].notnull().sum() / len(file) * 100) < 80: # если < 80% "NA"
+        del file[i] # удаление стоблца
     else:
-        file[i] = file[i].fillna(file[i].value_counts().idxmax())
+        file[i] = file[i].fillna(file[i].value_counts().idxmax()) # замена всех "NA" столбца на самое популярное значение в нем
 
-#file = file.fillna(method='pad')
+# file = file.fillna(method = "pad") замена всех "NA" кастомным способом
+# file_obj = file.select_dtypes(include = ["object"]).copy() копируем поля object в новый data set                                       ЗАЧЕМ?
+# for i in file.select_dtypes(include = ["object"]): по объектным столбцам
 
-# file_obj = file.select_dtypes(include=['object']).copy() # копируем поля object в новый data set
+file_obj = file.select_dtypes(include = ["object"]) # создаем file_obj, где остаются только объектные столбцы
+file_obj = file_obj.apply(lambda x: d[x.name].fit_transform(x)) # трансформация
 
-#for i in file.select_dtypes(include=['object']):
-file_obj = file.select_dtypes(include=['object'])
-#file_obj = file
-
-print(file_obj["LotShape"])
-
-file_obj = file_obj.apply(lambda x: d[x.name].fit_transform(x))
-
-print(file_obj["LotShape"])
-
-file_obj = file_obj.apply(lambda x: d[x.name].inverse_transform(x))
-
-print(file_obj["LotShape"])
+# file_obj = file_obj.apply(lambda x: d[x.name].inverse_transform(x)) обратная трансформация
