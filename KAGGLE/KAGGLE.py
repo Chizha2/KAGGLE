51ea5d3e4a801ac5d090c6dd0b0e63fb779cae02
dgok50 <dgok50@gmail.com>
2018-04-08 00:30:16 +0300

    kuip data as test train

--- KAGGLE/KAGGLE.py
+++ KAGGLE/KAGGLE.py
@@ -1,18 +1,49 @@
 from imports import * # импорт пакетов и модулей
 from functions import * # импорт функций
 warnings.filterwarnings("ignore") # отключение предупреждений
-file = pd.read_csv("../zadanie/train.csv", na_values = "NA") # чтение файла, пустые значения = "NA"
+file = pd.read_csv("../zadanie/kuip_train.csv", na_values = "NA") # чтение файла, пустые значения = "NA"
 file = NA_filter(file) # удаление лишних фич и замена "NA"
-chlst, price2 = graph_data(file)
-graph_print(chlst, price2) # получение данных для графика и его вывод
-price, space = graph_data2(file)
-graph_print2(price, space, chlst, price2) # получение данных для графика и его вывод
+#result, price = graph_data(file) # получение данных для графика
+#graph_print(result, price) # вывод графика
 code = LabelEncoder() # словарь для кодировки
 file = to_categorial(file, code) # перевод категориальных фич в числовые
-storage = preprocessing.scale(file) # стандартизация данных (перевод в хранилище)
-train, test =  train_test_split(storage,  train_size = 0.9) # разделение на обучающую и валидационную выборку
-storage = overturn(storage) # переворот матрицы
-x_datas, y = data_share(storage) # разделение данных
-model = lm.LinearRegression() # создание модели
-model.fit(np.transpose(np.matrix(x_datas)), np.transpose(np.matrix(y))) # обучение модели
-print(model.intercept_, model.coef_) # вывод параметров модели
+
+# storage = preprocessing.scale(file) # стандартизация данных (перевод в хранилище)
+# train, test = train_test_split(storage, train_size = 0.9) # разделение на обучающую и валидационную выборку
+# storage = overturn(storage) # переворот матрицы
+# x_datas, y = data_share(storage) # разделение данных
+# model = lm.LinearRegression() # создание модели
+# model.fit(np.transpose(np.matrix(x_datas)), np.transpose(np.matrix(y))) # обучение модели
+# print(model.intercept_, model.coef_) # вывод параметров модели
+
+#x_train, x_test, y_train, y_test = train_test_split(file.drop(columns = ['SalePrice']), file['SalePrice'], test_size = 0.25) # разделение на 4 части (2 тестовых и 2 валидационных)
+x_train, x_test, y_train, y_test = train_test_split(file.drop(columns = ['LUX']), file['LUX'], test_size = 0.25) # разделение на 4 части (2 тестовых и 2 валидационных)
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+from sklearn.preprocessing import StandardScaler
+x_scaler = StandardScaler().fit(x_train)
+y_scaler = StandardScaler().fit(y_train)
+x_train = x_scaler.transform(x_train)
+y_train = y_scaler.transform(y_train)
+x_test = x_scaler.transform(x_test)
+y_test = y_scaler.transform(y_test)
+from sklearn import linear_model
+model = linear_model.LinearRegression()
+model.fit(x_train, y_train)
+
+predictions = model.predict(x_test)
+
+y_predict = y_scaler.inverse_transform(predictions)
+y_real = y_scaler.inverse_transform(y_test)
+
+print(y_predict)
+print("---------------------------------------------------------------------------------------------")
+print(y_real)
+from sklearn.metrics import mean_absolute_error
+print(mean_absolute_error(y_real, y_predict))
+print(rmsle(y_real, y_predict))
+graph_print2(y_real, y_predict)
+p.scatter(y_scaler.inverse_transform(y_test), y_scaler.inverse_transform(predictions))
+p.xlabel("True Values")
+p.ylabel("Predictions")
+p.show()
--- KAGGLE/functions.py
+++ KAGGLE/functions.py
@@ -18,24 +18,18 @@ def graph_data(file): # получение данных для графика
     for i in range(len(price)): # по строкам
         k += 1 # увеличение счетчика
         f += square[i] / price[i] # занесение результата
-        if k % 1 == 0: # условие усреднения
-            result.append(f / 1) # добавление результата
+        if k % 10 == 0: # условие усреднения
+            result.append(f / 10) # добавление результата
             f = 0 # обнуление счетчика
-    k = 0
-    price_2 = []
-    for i in range(len(price)):
-        k += 1
-        f += price[i]
-        if k % 1 == 0:
-            price_2.append(f / 1)
-            f = 0
+    k = 0 # обнуление счетчика
+    price_2 = [] # пустой список для цен
+    for i in range(len(price)): # по строкам
+        k += 1 # увеличить счетчик
+        f += price[i] # изменение промежуточного значения
+        if k % 10 == 0: # каждые 10 элементов
+            price_2.append(f / 10) # поместить в результат
+            f = 0 # обнулить
     return result, price_2 # вернуть данные для графика
-
-def graph_data2(file): # получение данных для графика
-    file = file.sort_values("SalePrice") # сортировка по цене
-    pricelst = file["SalePrice"].rolling(1, min_periods=1, win_type='boxcar').sum().tolist() #подготовка данных для графика и преобразование в лист
-    spacelst = file["GrLivArea"].rolling(1, min_periods=1, win_type='boxcar').sum().tolist() # преобразование в лист
-    return pricelst, spacelst # вернуть данные для графика
 
 def graph_print(result, price_2): # вывод графика
     p.Figure() # создание фигуры
@@ -44,15 +38,14 @@ def graph_print(result, price_2): # вывод графика
     p.plot(price_2, result, label = "Соотношение", color = "r") # график
     p.show() # отображение фигуры
 
-def graph_print2(price, space, chlst, price2): # вывод графика
+def graph_print2(real, predict): # вывод графика
     p.Figure() # создание фигуры
-    figure, bars = p.subplots(2)
     p.title("Цена / площадь") # заголовок графика
     p.gcf().canvas.set_window_title("Цена / площадь") # название окна
-    bars[0].plot(price, space, label = "jok", color = "r", alpha=0.7) # график
-    bars[1].plot(price2, chlst, label="chizha", color="g", alpha=0.7)
-    bars[0].ticklabel_format(useOffset=False)
-    bars[0].ticklabel_format(style='plain')
+    p.plot(real, label = "real", color = "r", alpha=0.7) # график
+    p.plot(predict, label = "predict", color = "b", alpha=0.7) # график
+    p.ticklabel_format(useOffset=False)
+    p.ticklabel_format(style='plain')
     p.legend(mode="expand", borderaxespad=0)
     p.show() # отображение фигуры
 
@@ -75,3 +68,13 @@ def data_share(storage): # разделение данных
     x_datas = storage[:-1] # отделение данных
     y = storage[-1] # отделение цены
     return x_datas, y # вернуть 2 набора данных
+
+def rmsle(real, predicted):
+    sum=0.0
+    for x in range(len(predicted)):
+        if predicted[x]<0 or real[x]<0: #check for negative values
+            continue
+        p = np.log(predicted[x]+1)
+        r = np.log(real[x]+1)
+        sum = sum + (p - r)**2
+    return (sum/len(predicted))**0.5
