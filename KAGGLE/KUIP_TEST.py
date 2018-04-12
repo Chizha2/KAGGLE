a133f6391a93f56bebd4b703b1266d60598a1589
Chizh <Chizha2@gmail.com>
2018-04-12 18:54:15 +0300

    value rename

--- KAGGLE/KUIP_TEST.py
+++ KAGGLE/KUIP_TEST.py
@@ -6,23 +6,23 @@ splitd = 2
 
 file = pd.read_csv("../zadanie/kuip_train.csv", na_values = "NA") # чтение файла, пустые значения = "NA", kuip
 
-file = NA_filter(file, y_name = 'LUX') # удаление лишних фич и замена "NA"
-#result, price = graph_data(file) # получение данных для графика
-#graph_print(result, price) # вывод графика
+file = NA_filter(file, y_name = "LUX") # удаление лишних фич и замена "NA"
+# result, price = graph_data(file) получение данных для графика
+# graph_print(result, price) вывод графика
 file = to_categorial(file) # перевод категориальных фич в числовые
 
 
 if(splitd == 1):
-    x_train, x_test, y_train, y_test = train_test_split(file.drop(columns = ['LUX']), file['LUX'], test_size = 0.40) # разделение на 4 части (2 тестовых и 2 валидационных), kuip
+    x_train, x_test, y_train, y_test = train_test_split(file.drop(columns = ["LUX"]), file["LUX"], test_size = 0.4) # разделение на 4 части (2 тестовых и 2 валидационных), kuip
 
 if(splitd == 2):
-    splint_cof = int(len(file)/2) # расчёт и округление половины строк
+    splint_cof = int(len(file) / 2) # расчёт и округление половины строк
     x_train = file.head(splint_cof) # отделение 1 части от массива, с начала
-    x_test = file.tail(len(file)-splint_cof) # отделение 2 части, с конца
-    y_train = x_train['LUX'] # отделение y части
-    y_test = x_test['LUX'] # отделение y части
-    x_train = x_train.drop(columns = ['LUX']) # оставляем только x части
-    x_test = x_test.drop(columns = ['LUX']) # оставляем только x части
+    x_test = file.tail(len(file) - splint_cof) # отделение 2 части, с конца
+    y_train = x_train["LUX"] # отделение y части
+    y_test = x_test["LUX"] # отделение y части
+    x_train = x_train.drop(columns = ["LUX"]) # оставляем только x части
+    x_test = x_test.drop(columns = ["LUX"]) # оставляем только x части
     
 
 y_train = y_train.reshape(-1, 1) # фикс
@@ -46,7 +46,7 @@ print("Linear")
 y_predict = y_scaler.inverse_transform(model.predict(x_test)) # Предсказание для линейной модели и инвертирование трансформации
 y_predict = list(map(lambda x: x * (-1) if x < 0 else x, y_predict)) # дешифровка предположений
 
-mae_linear=int(mean_absolute_error(y_test, y_predict)) # Расчёт MAE
+mae_linear = int(mean_absolute_error(y_test, y_predict)) # Расчёт MAE
 rmsle_linear = rmsle(y_test, y_predict) # Расчёт RMSLE
 print(mae_linear) # вывод MAE
 print(rmsle_linear) # вывод RMSLE
@@ -54,7 +54,7 @@ print(rmsle_linear) # вывод RMSLE
 print("SGD")
 y_predict_sgd = y_scaler.inverse_transform(modelsgd.predict(x_test)) # Предсказание для SGD модели и инвертирование трансформации
 y_predict_sgd = list(map(lambda x: x * (-1) if x < 0 else x, y_predict_sgd)) # дешифровка предположений
-mae_sgd=int(mean_absolute_error(y_test, y_predict_sgd)) # Расчёт MAE
+mae_sgd = int(mean_absolute_error(y_test, y_predict_sgd)) # Расчёт MAE
 rmsle_sgd = rmsle(y_test, y_predict_sgd) # Расчёт RMSLE
 
 print(mae_sgd) # вывод MAE
@@ -63,12 +63,12 @@ print(rmsle_sgd) # вывод RMSLE
 p.Figure() # создание фигуры
 p.title("Сравнение типов регрессий") # заголовок графика
 p.gcf().canvas.set_window_title("Regrassions types compare") # название окна
-p.plot(y_test, label="real", color="r", alpha=0.7)  # график
-p.plot(y_predict, label="predict liner, mae:"+str(mae_linear)+", rmsle:"+str(rmsle_linear), color="b", alpha=0.7)  # график
-p.plot(y_predict_sgd, label="predict sgd, mae:"+str(mae_sgd)+", rmsle:"+str(rmsle_sgd), color="g", alpha=0.7)  # график
-p.ticklabel_format(useOffset=False)
-p.ticklabel_format(style='plain')
-p.legend(mode="expand", borderaxespad=0)
+p.plot(y_test, label = "real", color = "r", alpha = 0.7)  # график
+p.plot(y_predict, label = "predict liner, mae:" + str(mae_linear) + ", rmsle:" + str(rmsle_linear), color = "b", alpha = 0.7) # график
+p.plot(y_predict_sgd, label = "predict sgd, mae:" + str(mae_sgd) + ", rmsle:" + str(rmsle_sgd), color = "g", alpha = 0.7) # график
+p.ticklabel_format(useOffset = False)
+p.ticklabel_format(style = "plain")
+p.legend(mode = "expand", borderaxespad = 0)
 p.show() # отображение фигуры
 
 p.scatter(y_test, y_predict)
--- KAGGLE/TEST.py
+++ KAGGLE/TEST.py
@@ -2,8 +2,9 @@ from imports import * # импорт пакетов и модулей
 from functions import * # импорт функций
 warnings.filterwarnings("ignore") # отключение предупреждений
 
-print(" 1.KAGGLE\n 2.KUIP\n") # даём пользователю выбрать
-data_code = int(input("Выбирите набор данных:")) # Выбор набора данных
+print("1. KAGGLE\n2. KUIP") # даём пользователю выбрать
+data_code = int(input("Выберите набор данных: ")) # Выбор набора данных
+print()
 
 if(data_code == 1): # в случае выбора набора KAGGLE
     file = pd.read_csv("../zadanie/train.csv", na_values = "NA") # чтение файла, пустые значения = "NA", kaggle
@@ -14,8 +15,16 @@ elif(data_code == 2): # в случае выбора набора KUIP
 else:
     exit() # в остальных случаях
     
-print(" 1.train_test_split\n 2.Пополам\n") # даём пользователю выбрать
-splitd = int(input("Выберите тип разделения:")) # Выбор типа разделения
+proportion = float(input("Доля обучающей выборки: ")) # Выбор типа разделения
+print()
+
+random = input("Включить рандом? ") # Выбор типа разделения
+print()
+
+if (random != "no"):
+    random = True
+else:
+    random = False
 
 file = NA_filter(file, y_name = y_col_name) # удаление лишних фич и замена "NA"
 # result, price = graph_data(file) # получение данных для графика
@@ -31,33 +40,35 @@ errors = 0 # ошибки
 
 for j in range(100): # цикл
     model = linear_model.LinearRegression() # создание модели
-    
-    if(splitd == 1): # в случае выбора метода разделения train_test_split
-        x_train, x_test, y_train, y_test = train_test_split(file.drop(columns = [y_col_name]), file[y_col_name], test_size = 0.2) # разделение на 4 части (2 тестовых и 2 валидационных)
-    if(splitd == 2): # в случае выбора разделение попалам
-        splint_cof = int(len(file)/2) # расчёт и округление половины строк
-        x_train = file.head(splint_cof) # отделение 1 части от массива, с начала
-        x_test = file.tail(len(file)-splint_cof) # отделение 2 части, с конца
-        y_train = x_train[y_col_name] # отделение y части
-        y_test = x_test[y_col_name] # отделение y части
-        x_train = x_train.drop(columns = [y_col_name]) # оставляем только x части
-        x_test = x_test.drop(columns = [y_col_name]) # оставляем только x части
-    
-    y_train = y_train.reshape(-1, 1) # фикс
-    y_test = list(y_test)
+  
+    options_1, options_2, result_1, result_2 = train_test_split(file.drop(columns = [y_col_name]), file[y_col_name], test_size = proportion, shuffle = random)
 
-    x_scaler = StandardScaler().fit(x_train) # скейлер для X
-    y_scaler = StandardScaler().fit(y_train) # скейлер для Y
-    x_train = x_scaler.transform(x_train) # скайлирование X тренировки
-    y_train = y_scaler.transform(y_train) # скайлирование Y тренировки
-    x_test = x_scaler.transform(x_test) # скайлирование X тестирования
-    model.fit(x_train, y_train) # обучение модели
-    predictions = model.predict(x_test) # предположения
-    predictions = list(map(lambda x: x * (-1) if x < 0 else x, y_scaler.inverse_transform(predictions))) # дешифровка предположений
-    if (mean_absolute_error(y_test, predictions) < 50000): # ошибки нет
+#    if(splitd == 1): # в случае выбора метода разделения train_test_split
+#        x_train, x_test, y_train, y_test = train_test_split(file.drop(columns = [y_col_name]), file[y_col_name], test_size = 0.2) # разделение на 4 части (2 тестовых и 2 валидационных)
+#    if(splitd == 2): # в случае выбора разделение попалам
+#        splint_cof = int(len(file)/2) # расчёт и округление половины строк
+#        x_train = file.head(splint_cof) # отделение 1 части от массива, с начала
+#        x_test = file.tail(len(file)-splint_cof) # отделение 2 части, с конца
+#        y_train = x_train[y_col_name] # отделение y части
+#        y_test = x_test[y_col_name] # отделение y части
+#        x_train = x_train.drop(columns = [y_col_name]) # оставляем только x части
+#        x_test = x_test.drop(columns = [y_col_name]) # оставляем только x части
+    
+    result_1 = result_1.reshape(-1, 1) # фикс
+    result_2 = list(result_2)
+
+    options_scaler = StandardScaler().fit(options_1) # скейлер для X
+    result_scaler = StandardScaler().fit(result_1) # скейлер для Y
+    options_1 = options_scaler.transform(options_1) # скайлирование X тренировки
+    result_1 = result_scaler.transform(result_1) # скайлирование Y тренировки
+    options_2 = options_scaler.transform(options_2) # скайлирование X тестирования
+    model.fit(options_1, result_1) # обучение модели
+    predictions = model.predict(options_2) # предположения
+    predictions = list(map(lambda x: x * (-1) if x < 0 else x, result_scaler.inverse_transform(predictions))) # дешифровка предположений
+    if (mean_absolute_error(result_2, predictions) < 50000): # ошибки нет
         k += 1 # увеличение счетчика
-        rmsle_k += rmsle(y_test, predictions) # rmsle
-        mae_k += mean_absolute_error(y_test, predictions) # mae
+        rmsle_k += rmsle(result_2, predictions) # rmsle
+        mae_k += mean_absolute_error(result_2, predictions) # mae
     else: # иначе
         errors += 1 # + ошибка
 
